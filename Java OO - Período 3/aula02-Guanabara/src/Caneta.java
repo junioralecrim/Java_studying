046f50e2a151e1e8082875022cbb9bd34d29b1e4
Junior Alecrim <junioralecrim.16@gmail.com>
2022-08-15 15:53:25 -0300

    começando a mexer com objetos, classes e métodos

---
+++ Java OO - Período 3/aula02-Guanabara/src/Caneta.java
@@ -0,0 +1,19 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void rabiscar(){
+
+    }
+
+    void tampar(){
+
+    }
+
+    void destampar(){
+        
+    }
+}
