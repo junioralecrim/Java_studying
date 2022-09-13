b0acdebc25fc276ee229de03ec0839ae0de9945b
Junior Alecrim <junioralecrim.16@gmail.com>
2022-09-13 14:01:17 -0300

    Estudando relacionamento de agregação JAVA

---
+++ Java OO - Período 3/AULA Teoria 07 - Relacionamento entre classes/src/Luta.java
@@ -0,0 +1,52 @@
+public class Luta {
+
+    //Eu vou estar criando logo abaixo um relacionamento com a classe Lutador.
+
+    //RELACIONAMENTO DO TIPO "TEM UM"
+    //A classe Luta "TEM UM" (que na verdade é mais de um) ATRIBUTO que é da classe Lutador.
+
+    private Lutador desafiado; //vai ser uma instância da classe lutador. Ou seja, vai ser um objeto da classe lutador.
+    //Aqui eu estou fazendo a definição do atributo como sendo do tipo abstrato de dado
+
+    private Lutador desafiante; //outra instância da classe lutador. Agora eu tenho o objeto desafiante.
+    private int rounds;
+    private boolean aprovada;
+
+
+    public void marcarLuta(Lutador l1, Lutador l2){
+        //se a categoria for igual e os lutadores diferentes
+        if ((l1.getCategoria() == l2.getCategoria()) && (l1 != l2)){
+            aprovada = true;
+            desafiado = l1;
+            desafiante = l2;
+        } else {
+            aprovada = false;
+            desafiado = null;
+            desafiante = null;
+        }
+    }
+
+    public void lutar(){
+        if (aprovada){
+            desafiado.apresentar(); //COMO DESAFIADO É UM OBJETO DO TIPO LUTADOR, eu posso usar os métodos dessa classe
+            //sem problemas.
+
+            desafiante.apresentar();
+            int vencedor = 1; //AINDA PRECISO CRIAR UM GERADOR DE NÚMERO ALEATÓRIO
+            
+
+
+
+        } else {
+            System.out.println("Luta não pode acontecer");
+        }
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+}
--- Java OO - Período 3/AULA Teoria 07 - Relacionamento entre classes/src/Lutador.java
+++ Java OO - Período 3/AULA Teoria 07 - Relacionamento entre classes/src/Lutador.java
@@ -1,14 +1,14 @@
 public class Lutador {
     //metodo construtor
-    public Lutador(String no, String nacio, Float altu, int ida, String pe, int vito, int derro, int empa){
-        nome = no;
-        nacionalidade = nacio;
-        altura = altu;
-        idade = ida;
-        setPeso(peso);
-        vitorias = vito;
-        derrotas = derro;
-        empates = empa;
+    public Lutador(String no, String nacio, int ida, Float altu, Float pe, int vito, int derro, int empa){
+        this.nome = no;
+        this.nacionalidade = nacio;
+        this.altura = altu;
+        this.idade = ida;
+        setPeso(pe);
+        this.vitorias = vito;
+        this.derrotas = derro;
+        this.empates = empa;
 
     }
 
@@ -17,30 +17,42 @@ public class Lutador {
     private String nacionalidade;
     private Float altura;
     private int idade;
-    private String peso;
+    private Float peso;
     private String categoria;
     private int vitorias;
     private int derrotas;
     private int empates;
 
     public void apresentar(){
-
+        System.out.println("---------------------------------------------");
+        System.out.println("Lutador: " + this.getNome());
+        System.out.println("Origem: " + this.getNacionalidade());
+        System.out.println(this.getIdade() + " anos");
+        System.out.println(this.getAltura() + "m de altura");
+        System.out.println("Pesando " + this.getPeso() + "kg");
+        System.out.println("Ganhou " + this.getVitorias());
+        System.out.println("Perdeu " + this.getDerrotas());
+        System.out.println("Empatou " + this.getEmpates());
     }
 
     public void status(){
-
+        System.out.println(this.getNome());
+        System.out.println("É um peso: " + this.getCategoria());
+        System.out.println("Vitórias: " + this.getVitorias());
+        System.out.println("Derrotas: " + this.getDerrotas());
+        System.out.println("Empates: " + this.getEmpates());
     }
 
     public void ganharLuta(){
-
+        this.setVitorias(this.getVitorias() + 1);
     }
 
     public void perderLuta() {
-
+        this.setDerrotas(this.getDerrotas() + 1);
     }
 
     public void empatarLuta(){
-
+        this.setEmpates(this.getEmpates() + 1);
     }
 
     //métodos especiais
@@ -54,16 +66,78 @@ public class Lutador {
         this.nome = no;
     }
 
-    public String getPeso() {
-        return peso;
-    }
-
-    public void setPeso(String pe) {
+    public void setPeso(Float pe) {
         this.peso = pe;
         setCategoria();
     }
 
-    public void setCategoria(String cate) {
-        this.categoria = categoria;
+    public Float getPeso() {
+        return peso;
+    }
+
+    private void setCategoria() {
+        if(this.peso < 52.2){
+            this.categoria = "Inválido";
+        } else if (this.peso <= 70.3) {
+            this.categoria = "Leve";
+        } else if (this.peso <= 83.9){
+            this.categoria = "Médio";
+        } else if (this.peso <= 120.2){
+            this.categoria = "Pesado";
+        } else {
+            this.categoria = "Inválido";
+        }
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setAltura(Float altura) {
+        this.altura = altura;
+    }
+
+    public Float getAltura() {
+        return altura;
     }
 }
