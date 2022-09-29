f885799da0b4fcf51f16a3fd3b4dbdfed84799b7
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-09-28 21:50:21 -0300

    Update Main.java

--- Java OO - Período 3/contaBanco/src/Main.java
+++ Java OO - Período 3/contaBanco/src/Main.java
@@ -20,50 +20,52 @@ public class Main {
                 "0 - Sair do sistema ");
         int opcao = in.nextInt();
 
+        do {
         switch (opcao) {
-
             case 1:
-            System.out.print("Seu nome: ");
-            String nome = in.nextLine();
-            limparbuff(in);
-            System.out.print("CPF: ");
-            String cpf = in.next();
-            System.out.print("Digite o tipo de conta que você deseja\nCC - Conta corrente\nCP - Conta poupança\n ");
-            String tipo = in.next();
-            System.out.print("Agora, digite uma senha de 4 dígitos: ");
-            int senha = in.nextInt();
-            contaPessoa.abrirConta(nome, tipo, cpf, senha);
+                System.out.print("Seu nome: ");
+                String nome = in.nextLine();
+                limparbuff(in);
+                System.out.print("CPF: ");
+                String cpf = in.next();
+                System.out.print("Digite o tipo de conta que você deseja\nCC - Conta corrente\nCP - Conta poupança\n ");
+                String tipo = in.next().toLowerCase();
+                System.out.print("Agora, digite uma senha de 4 dígitos: ");
+                int senha = in.nextInt();
+                contaPessoa.abrirConta(nome, tipo, cpf, senha);
                 break;
 
             case 2:
-            System.out.println("Ainda em desenvolvimento...");
+                contaPessoa.fecharConta();
 
                 break;
 
             case 3:
-            System.out.println("Ainda em desenvolvimento...");
+                contaPessoa.depositar();
 
                 break;
 
             case 4:
-            System.out.println("Ainda em desenvolvimento...");
+                System.out.println("Ainda em desenvolvimento...");
 
                 break;
 
             case 5:
-            System.out.println("Ainda em desenvolvimento...");
+                System.out.println("Ainda em desenvolvimento...");
 
                 break;
 
             case 0:
-            System.out.println("Ainda em desenvolvimento...");
+                System.out.println("Ainda em desenvolvimento...");
                 break;
 
 
 
             default:
-            System.out.println("Ainda em desenvolvimento...");
+                System.out.println("Ainda em desenvolvimento...");
 
         }
+        } while (opcao != 0);
+
     }
 }
