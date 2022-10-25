e7f753e0a5c30ef88bf1604ecbd71b13dbd1cdab
Junior Alecrim <junioralecrim.16@gmail.com>
2022-10-25 16:05:46 -0300

    Finalizada a parte de injeção de dependências

--- Spring Boot [Framework Java]/Module 1 - Spring Boot Core and Spring Framework/vendas/src/main/java/com/vendas/service/ClientesService.java
+++ Spring Boot [Framework Java]/Module 1 - Spring Boot Core and Spring Framework/vendas/src/main/java/com/vendas/service/ClientesService.java
@@ -2,15 +2,22 @@ package com.vendas.service;
 
 import com.vendas.model.Cliente;
 import com.vendas.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientesService {
 
+    private ClientesRepository repository;
+
+    @Autowired
+    public ClientesService(ClientesRepository repository) {
+        this.repository = repository;
+    } /* INJEÇÃO DA DEPENDÊNCIA NO CONTAINER DO SPRING BOOT*/
+
     public void salvarCliente (Cliente cliente){
         validarCliente(cliente);
-        ClientesRepository clientesRepository = new ClientesRepository();
-        clientesRepository.persistir(cliente);
+        this.repository.persistir(cliente); /**/
     }
 
     public void validarCliente(Cliente cliente){
