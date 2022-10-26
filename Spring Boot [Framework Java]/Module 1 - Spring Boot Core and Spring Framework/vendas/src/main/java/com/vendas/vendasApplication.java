530312665a6fecce42c1f89b67236131d01f327d
Junior Alecrim <junioralecrim.16@gmail.com>
2022-10-26 14:27:47 -0300

    erro de multiplos ambientes: corrigido
    O erro era basicamente no nome, que deveria ser Application e não "Aplication", como estava.

--- Spring Boot [Framework Java]/Module 1 - Spring Boot Core and Spring Framework/vendas/src/main/java/com/vendas/vendasApplication.java
+++ Spring Boot [Framework Java]/Module 1 - Spring Boot Core and Spring Framework/vendas/src/main/java/com/vendas/vendasApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.ComponentScan;
