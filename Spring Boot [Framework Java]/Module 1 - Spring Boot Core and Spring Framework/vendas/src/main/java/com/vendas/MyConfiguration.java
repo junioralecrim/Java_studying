2e483b4082c3378ad8f0f5b1d24d93a4143890a0
Junior Alecrim <junioralecrim.16@gmail.com>
2022-10-26 15:33:32 -0300

    finalizada parte de application.properties

--- Spring Boot [Framework Java]/Module 1 - Spring Boot Core and Spring Framework/vendas/src/main/java/com/vendas/MyConfiguration.java
+++ Spring Boot [Framework Java]/Module 1 - Spring Boot Core and Spring Framework/vendas/src/main/java/com/vendas/MyConfiguration.java
@@ -1,9 +1,17 @@
 package com.vendas;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration /* É uma annotation que indica que determinada classe possui métodos que expõe
 novos beans e configs. */
+@Profile("development")
 public class MyConfiguration {
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args-> {
+            System.out.println("RODANDO A CONFIG DE DESENVOLVIMENTO");
+        };
+    }
 }
