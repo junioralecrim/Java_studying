3d5de5ced8779029b628c85c45e4a98a441306f7
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2023-03-17 01:15:03 -0300

    update

---
+++ Trilha Java Alura/Java Collections/2 - Listas de objetos/arrayList/src/main/java/org/example/Teste.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Teste {
+    private enum teste{
+        T,
+        J
+    }
+}
+
+
---
+++ Trilha Java Alura/Java Collections/2 - Listas de objetos/arrayList/src/main/java/org/example/testTest.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class testTest {
+    public static void main(String[] args) {
+        Teste teste = new Teste();
+
+    }
+}
